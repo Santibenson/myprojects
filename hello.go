bbfd5ee4a27eefb9f0c52de2db35f823cbc7013e
santibenson <bensonoluchukwuisaac@gmail.com>
2022-12-21 13:57:43 +0100

    Second commit showing main var types in golang

--- hello.go
+++ hello.go
@@ -4,5 +4,27 @@ import("fmt")
 func main(){
 	var greeting string = "Hello, Go. You're welcome"
 
-	fmt.print(greeting)
+	fmt.PrintIn(greeting)
 }
+
+
+package main
+
+import ("fmt")
+
+func main()  {
+	var (
+	name string = "Benson"
+	age int = 100
+	money float32 = 2.90
+	)
+
+	fmt.Println("My name is " + name + ". I am",age, "years old. This is a", money,"float.")
+}
+
+// I want to comment
+
+/* I want a multi-line
+comment to be here
+anyways
+*/
